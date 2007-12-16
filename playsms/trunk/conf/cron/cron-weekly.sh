d62c04e5b7b9da4ea551d07e1ac9ac3bd9c2baca
mithrandir <mithrandir@f8cd03ff-103d-0410-bb72-81d3b1e9bcfd>
2007-12-16 00:06:57 +0000

    modified the cron conf files to be more easily maintained

--- playsms/trunk/conf/cron/cron-weekly.sh
+++ playsms/trunk/conf/cron/cron-weekly.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-
-# this should go in /etc/cron.weekly
-when=weekly
-curl "http://localhost/playsms/menu_admin.php?inc=sms_autosend&op=autosend&when=$when"
+/etc/playsms/cron/cron.sh weekly
 
---
+++ playsms/trunk/conf/cron/cron.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+when=$1
+curl "http://localhost/playsms/inc/feat/sms_autosend.php?op=autosend&when=$when"
+
