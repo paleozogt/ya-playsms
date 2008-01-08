adaaca6eb5a69db15e358e9fdc5f6923caa5be5a
mithrandir <mithrandir@f8cd03ff-103d-0410-bb72-81d3b1e9bcfd>
2008-01-08 07:50:18 +0000

    changed autosend 'when' field to 'frequency' bc 'when' appears to be a reserved word in mysql

--- playsms/trunk/conf/cron/cron.sh
+++ playsms/trunk/conf/cron/cron.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 when=$1
-curl "http://localhost/playsms/inc/feat/sms_autosend.php?op=autosend&when=$when"
+curl "http://localhost/playsms/inc/feat/sms_autosend.php?op=autosend&frequency=$when"
 
